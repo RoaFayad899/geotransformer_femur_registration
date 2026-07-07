6b6d763b8a9806bedb8e912d886283e5929fd84f
Roa Fayad <rf.phys2@gmail.com>
2026-07-07 11:28:31 +0200

    New test for overfitting test

--- experiments/geotransformer.femur.stage4.gse.k3.max.oacl.stage2.sinkhorn/config.py
+++ experiments/geotransformer.femur.stage4.gse.k3.max.oacl.stage2.sinkhorn/config.py
@@ -152,7 +152,7 @@ _C.geotransformer.reduction_a = 'max'
 # model - Fine Matching
 _C.fine_matching = edict()
 _C.fine_matching.topk = 3
-_C.fine_matching.acceptance_radius = mm_to_norm(6.0)                    ###################0.1, 12.0
+_C.fine_matching.acceptance_radius = mm_to_norm(12.0)                    ###################0.1, 12.0
 _C.fine_matching.mutual = True
 _C.fine_matching.confidence_threshold = 0.05
 _C.fine_matching.use_dustbin = False
@@ -172,7 +172,7 @@ _C.coarse_loss.positive_overlap = 0.1
 
 # loss - Fine level
 _C.fine_loss = edict()
-_C.fine_loss.positive_radius = mm_to_norm(3.0)                 #############################0.05, 6.0
+_C.fine_loss.positive_radius = mm_to_norm(6.0)                 #############################0.05, 6.0
 
 # loss - Overall
 _C.loss = edict()
