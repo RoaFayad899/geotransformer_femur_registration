21e62435d86a1c5d6fd00a4179f224b3938829f3
Roa Fayad <rf.phys2@gmail.com>
2026-07-07 14:09:37 +0200

    Fix a problem

--- experiments/geotransformer.femur.stage4.gse.k3.max.oacl.stage2.sinkhorn/best_epoch_summary.py
+++ experiments/geotransformer.femur.stage4.gse.k3.max.oacl.stage2.sinkhorn/best_epoch_summary.py
@@ -1,7 +1,7 @@
 import os
 import pandas as pd
 
-LOG_DIR = "../../output/exp_test_overfit1_noaugmentation_lrconst_12/logs"     #############################
+LOG_DIR = "../../output/exp_test_overfit1_noaugmentation_lrconst_13/logs"     #############################
 
 csv_path = os.path.join(LOG_DIR, "parsed_metrics.csv")
 if not os.path.exists(csv_path):
