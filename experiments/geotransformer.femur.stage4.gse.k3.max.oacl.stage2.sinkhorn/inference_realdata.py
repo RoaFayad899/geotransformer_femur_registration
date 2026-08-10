1c746d00f06e70a29892e0cd1e96e6085ccb28cd
Roa Fayad <rf.phys2@gmail.com>
2026-08-10 17:20:30 +0200

    test

--- experiments/geotransformer.femur.stage4.gse.k3.max.oacl.stage2.sinkhorn/inference_realdata.py
+++ experiments/geotransformer.femur.stage4.gse.k3.max.oacl.stage2.sinkhorn/inference_realdata.py
@@ -22,7 +22,7 @@ from model import create_model
 # ============================================================
 
 INFERENCE_DATASET_NAME = (
-    "geotransformer_dataset_inference_USProximaltoCT"
+    "geotransformer_dataset_inference_USProximaltoCTComplete"
 )
 
 EXPERIMENT_NAME = (
