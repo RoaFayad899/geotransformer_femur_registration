6a1ba24136f851126d76a2ab8bd40bb0e395fa25
Roa Fayad <rf.phys2@gmail.com>
2026-08-18 16:27:14 +0200

    test

--- experiments/geotransformer.femur.stage4.gse.k3.max.oacl.stage2.sinkhorn/config.py
+++ experiments/geotransformer.femur.stage4.gse.k3.max.oacl.stage2.sinkhorn/config.py
@@ -16,7 +16,7 @@ _C.seed = 7351
 # dirs
 _C.working_dir = osp.dirname(osp.realpath(__file__))
 _C.root_dir = osp.dirname(osp.dirname(_C.working_dir))
-_C.exp_name = "exp_fulldataset_best4stages_2000_large_new_04"                                ###############################
+_C.exp_name = "exp_fulldataset_best4stages_8000_large_new_00"                                ###############################
 _C.output_dir = osp.join(_C.root_dir, 'output', _C.exp_name)
 _C.snapshot_dir = osp.join(_C.output_dir, 'snapshots')
 _C.log_dir = osp.join(_C.output_dir, 'logs')
@@ -36,7 +36,7 @@ _C.data = edict()
 _C.data.dataset_root = osp.join(
     _C.root_dir,
     "data",
-    "geotransformer_dataset_USlike_partial_to_full_femur_4000_large")                                ################################
+    "geotransformer_dataset_USlike_partial_to_full_femur_8000_large")                                ################################
 
 #_C.data.dataset_root = osp.join(_C.root_dir, 'data', 'Femur')
 
@@ -111,7 +111,7 @@ _C.optim.lr = 1e-4                    ###################
 _C.optim.lr_decay = 1.0              ###################
 _C.optim.lr_decay_steps = 1
 _C.optim.weight_decay = 1e-6
-_C.optim.max_epoch = 1                #################
+_C.optim.max_epoch = 500                #################
 _C.optim.grad_acc_steps = 1
 
 # model - backbone
